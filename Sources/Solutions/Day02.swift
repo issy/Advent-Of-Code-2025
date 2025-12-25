6c85fab684cd154bd94ad4a159439ab50df96a90
issy <issy@issy.dev>
2025-12-25 13:41:28 -0700

    Add day 2 solution stub

---
+++ Sources/Solutions/Day02.swift
@@ -0,0 +1,20 @@
+import protocol Lib.AdventDay
+
+public struct Day02: AdventDay {
+  public static let day = 2
+
+  public init() {}
+
+  public func parse(_ input: String) throws -> String {
+    ""
+  }
+
+  public func part1(_ input: String) -> Int {
+    0
+  }
+
+  public func part2(_ input: String) -> Int {
+    0
+  }
+
+}
